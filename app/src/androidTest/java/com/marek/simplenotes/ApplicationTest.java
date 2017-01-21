0fb8686a7b9aeee1e07d175bfab3be03ea536ff8
Marek Newton <marek.s.newton@gmail.com>
2017-01-21 15:25:12 -0700

    worked on some details

--- app/src/androidTest/java/com/marek/simplenotes/ApplicationTest.java
+++ app/src/androidTest/java/com/marek/simplenotes/ApplicationTest.java
@@ -3,6 +3,9 @@ package com.marek.simplenotes;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
--- app/src/main/java/com/marek/simplenotes/NewActivity.java
+++ app/src/main/java/com/marek/simplenotes/NewActivity.java
@@ -2,26 +2,34 @@ package com.marek.simplenotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import java.util.Date;
 import android.content.Intent;
+import android.widget.TextView;
 
 public class NewActivity extends AppCompatActivity {
 
     private static final String ID ="ID";
     private static final String USAGE = "USAGE";
+    private static final String ADD = "ADD";
+    private static final String UPDATE = "UPDATE";
     EditText noteTextInput;
     Note newNote;
     NoteOperations noteData;
     String usage;
     String id;
     Note oldNote;
+    TextView warningText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+
+        warningText = (TextView)findViewById(R.id.warningText);
 
         noteTextInput = (EditText)findViewById(R.id.noteText);
         newNote = new Note();
@@ -31,10 +39,23 @@ public class NewActivity extends AppCompatActivity {
         usage = getIntent().getStringExtra(USAGE);
         id = getIntent().getStringExtra(ID);
 
-        if(usage.equals("UPDATE")) {
+        if(usage.equals(UPDATE)) {
             oldNote = noteData.getNote(Long.parseLong(id));
             noteTextInput.setText(oldNote.getContent());
         }
+
+        noteTextInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                warningText.setText("");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
     }
 
     public void doneOnClick(View view) {
@@ -44,18 +65,25 @@ public class NewActivity extends AppCompatActivity {
         newNote.setContent(noteText);
         newNote.setDate(noteDate);
 
-        if(usage.equals("ADD")) {
-            noteData.addNote(newNote);
-        }
-        else if(usage.equals("UPDATE")) {
-            oldNote.setContent(noteText);
-            oldNote.setDate(noteDate);
+        if(noteText.length() > 0) {
+            if (usage.equals(ADD)) {
+                noteData.addNote(newNote);
+            } else if (usage.equals(UPDATE)) {
+                oldNote.setContent(noteText);
+                oldNote.setDate(noteDate);
 
-            noteData.updateNote(oldNote);
-        }
+                noteData.updateNote(oldNote);
+            }
 
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            warningText.setText(R.string.warning_text);
+        }
+    }
+
+    public void noOnClick(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 }
